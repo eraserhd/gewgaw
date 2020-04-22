4a0e132ed17ad85ffffb545b7c3efb8ecf7f8705
Jason Felice <jason.m.felice@gmail.com>
2020-04-22 12:21:17 -0400

    Add overlays to overlays array

--- decals.c
+++ decals.c
@@ -12,8 +12,23 @@ Class AppDelegateClass;
 typedef struct AppDelegate_tag
 {
     Class isa;
+    id overlays;
 }
 AppDelegate;
+
+id AppDelegate_init(AppDelegate *self, SEL _cmd)
+{
+    struct objc_super super =
+    {
+        (id)self,
+        objc_getClass("NSObject")
+    };
+    if ((self = (AppDelegate *)objc_msgSendSuper(&super, sel_getUid("init"))))
+    {
+        self->overlays = objc_msgSend(objc_msgSend((id) objc_getClass("NSMutableArray"), sel_getUid("alloc")), sel_getUid("init"));
+    }
+    return (id)self;
+}
 
 void AppDelegate_applicationDidFinishLaunching(AppDelegate *self, SEL _cmd, id notification)
 {
@@ -23,14 +38,14 @@ void AppDelegate_applicationDidFinishLaunching(AppDelegate *self, SEL _cmd, id n
         .origin = {100,100},
         .size = {200,200}
     };
-    id window = objc_msgSend(objc_msgSend((id) objc_getClass("NSWindow"), sel_getUid("alloc")),
+    id overlay = objc_msgSend(objc_msgSend((id) objc_getClass("NSWindow"), sel_getUid("alloc")),
                              sel_getUid("initWithContentRect:styleMask:backing:defer:"),
                              rect,
                              (1 << 0) | (1 << 1), /* titled, closable */
                              2, /* buffered */
                              NO);
-
-    objc_msgSend(window, sel_getUid("makeKeyAndOrderFront:"), Nil);
+    objc_msgSend(self->overlays, sel_getUid("addObject:"), overlay);
+    objc_msgSend(overlay, sel_getUid("makeKeyAndOrderFront:"), Nil);
 }
 
 void AppDelegate_applicationWillTerminate(AppDelegate *self, SEL _cmd, id notification)
@@ -40,7 +55,9 @@ void AppDelegate_applicationWillTerminate(AppDelegate *self, SEL _cmd, id notifi
 void init_AppDelegateClass()
 {
     AppDelegateClass = objc_allocateClassPair(objc_getClass("NSObject"), "AppDelegate", 0);
+    class_addIvar(AppDelegateClass, "overlays", sizeof(id), 0, "@");
     class_addProtocol(AppDelegateClass, objc_getProtocol("NSApplicationDelegate"));
+    class_addMethod(AppDelegateClass, sel_getUid("init"), (IMP) AppDelegate_init, "@@:");
     class_addMethod(AppDelegateClass, sel_getUid("applicationDidFinishLaunching:"), (IMP) AppDelegate_applicationDidFinishLaunching, "v@:@");
     class_addMethod(AppDelegateClass, sel_getUid("applicationWillTerminate:"), (IMP) AppDelegate_applicationWillTerminate, "v@:@");
     objc_registerClassPair(AppDelegateClass);
